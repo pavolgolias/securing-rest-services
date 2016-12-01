4590c5cfc05777af8a31127e713725f479d6bd4e
Andrej Kosar <andrejksr@gmail.com>
2016-12-01 22:01:50 +0100

    Created gradle project with needed dependencies

---
+++ src/main/java/sk/stu/fei/asos/SecuringRestServicesApplication.java
@@ -0,0 +1,12 @@
+package sk.stu.fei.asos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuringRestServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuringRestServicesApplication.class, args);
+	}
+}
